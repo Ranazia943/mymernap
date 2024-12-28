a16864a84f8e7a5e21168af058cdb6dbef96451e
Ranazia943 <ziar7750@gmail.com>
2024-12-28 09:02:26 -0800

    Delete frontend directory

--- frontend/src/authcontext/AdminProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { useAdminAuthContext } from "./AdminAuthContext";
-
-const AdminProtectedRoute = ({ children }) => {
-  const { adminUser } = useAdminAuthContext();
-  return adminUser ? children : <Navigate to="/admin/login" />;
-};
-
-export default AdminProtectedRoute;
--- frontend/src/authcontext/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { useAuthContext } from "./AuthContext";
-
-const ProtectedRoute = ({ children }) => {
-  const { authUser } = useAuthContext();
-  return authUser ? children : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;
